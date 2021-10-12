e5ea02a2ccdf71a58ab4280e087d7de76ab77d59
Awad Sharif <awadsharif9@gmail.com>
2021-10-12 00:13:02 -0700

    created coding quiz

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,7 +3,7 @@
 
 var startButton = document.querySelector("#start-button");
 var timerCount = document.querySelector(".timer");
-var timeLeft = 10;
+var timeLeft = 30;
 var timer;
 // logic for next button
 var nextButton = document.createElement("button");
@@ -20,7 +20,7 @@ var questions = [
     `<p>This is Question 5!</p>`
 ];
 
-// timer function for 10 second time limit
+// timer function for 30 second time limit
 // stop the timer if user either answers all 5 questions or fails to anser before time limit
 function timerStart() {
     timer = setInterval(function() {
@@ -52,11 +52,6 @@ nextButton.addEventListener("click", function() {
     if (currentQuestion > 1 && currentQuestion < 6) {
         document.querySelector("#stage").innerHTML = questions[currentQuestion];
         document.querySelector("#stage").appendChild(nextButton);
-    }
-    // the game is over if user either answers all the questions or time is up! 
-
-    if (currentQuestion > 5) {
-        document.querySelector("#stage").innerHTML = "<h1>GAME OVER!</h1>";
     }
 })
 
