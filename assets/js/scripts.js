d06323724ffc388e0f597d1692b1f80302620c33
Awad Sharif <awadsharif9@gmail.com>
2021-10-13 00:27:25 -0700

    added questions to coding quiz

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -34,14 +34,18 @@ initialsBox.setAttribute("id", "initials");
 // record which question the user is currently at
 var currentQuestion = 0;
 
+// record the score user gets
+var score = 120;
+var scoreTotal = document.createElement("span");
+
 // Questions and answers
 var questions = [
     "",
-    `<p>What is Javascript?</p> <div class="radio-choices"><button class="choiceBtn">GOD</button><button class="choiceBtn">I don't know</button><button class="choiceBtn">The knockoff Java</button><button class="choiceBtn">A programming language</button></div> `,
-    `<p>Inside which HTML element do we put the JavaScript?</p> <div class="radio-choices"><input type="radio" name="choice" id="correct-answer"><label for="rad1">javascript</label> <input type="radio" name="choice" id="wrong-answer"><label for="rad1">html</label>, <input type="radio" name="choice" id="wrong-answer"><label for="rad1">js</label> <input type="radio" name="choice"><label for="rad1" id="wrong-answer">script</label></div> `,
-    `<p>This is Question 3!</p>`,
-    `<p>This is Question 4!</p>`,
-    `<p>This is Question 5!</p>`
+    `<h2>What is Javascript?</h2> <div class="multiple-choices"><button class="choiceBtn">GOD</button><button class="choiceBtn">I don't know</button><button class="choiceBtn">The knockoff Java</button><button class="choiceBtn">A programming language</button></div> `,
+    `<h2>Which of the following is not a Javascript method?</h2> <div class="multiple-choices"><button class="choiceBtn">.split()</button><button class="choiceBtn">.join()</button><button class="choiceBtn">.print()</button><button class="choiceBtn">.includes()</button></div> `,
+    `<h2>How to declare a while loop to count from 0 to 10?</h2> <div class="multiple-choices"><button class="choiceBtn">while i=1 to 10</button><button class="choiceBtn">while (i <= 10)</button><button class="choiceBtn">while (i <= 10; i++)</button><button class="choiceBtn">while i > 0 && i < 11</button></div> `,
+    `<h2>How do you use DOM to select the ID element '#header'?</h2> <div class="multiple-choices"><button class="choiceBtn">document.selector("#header")</button><button class="choiceBtn">document.ID("#header")</button><button class="choiceBtn">document.ID = ("#header")</button><button class="choiceBtn">document.querySelector("#header")</button></div> `,
+    `<h2>What keyword is used to skip loop iteration?</h2> <div class="multiple-choices"><button class="choiceBtn">continue</button><button class="choiceBtn">break</button><button class="choiceBtn">next</button><button class="choiceBtn">skip</button></div> `
 ];
 
 
@@ -53,7 +57,7 @@ function timerStart() {
     var timerInterval = setInterval(function() {
         timerCount.textContent = timeLeft;
         if (timeLeft === 0 || currentQuestion > 5) {
-            document.querySelector("#stage").innerHTML = "<h1>GAME OVER!</h1>";
+            document.querySelector("#stage").innerHTML = "<h3>Your total score is: </h3>" + score;
             formSubmit.appendChild(initialsBox);
             formSubmit.appendChild(initialsButton);
             document.querySelector("#stage").appendChild(formSubmit);
